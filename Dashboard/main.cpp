2b199c9a78ed5eef71645f145b8be533b8ec9b00
Boonthai Yan <56100316+Thai-bit@users.noreply.github.com>
2021-06-14 15:39:36 -0700

    updated main

--- Dashboard/main.cpp
+++ Dashboard/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+     qDebug()<<QSslSocket::supportsSsl() << QSslSocket::sslLibraryBuildVersionString() << QSslSocket::sslLibraryVersionString();
     return a.exec();
 }
